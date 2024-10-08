1b33a5df8ca2d0a2ea56494b3e3fc0e5aee46fe7
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2024-10-08 20:51:44 -0300

    08/10/24

--- projeto_manutencao/src/main/java/com/example/controllers/MaquinaController.java
+++ projeto_manutencao/src/main/java/com/example/controllers/MaquinaController.java
@@ -1,5 +1,19 @@
 package com.example.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.models.Maquina;
+
 public class MaquinaController {
-    
+    private List<Maquina> maquinas;
+
+    public MaquinaController(){
+        maquinas = new ArrayList<>();
+    }
+
+    //metodos
+    public void createMaquina(Maquina maquina){
+        maquinas.add(maquina);
+    }
 }
